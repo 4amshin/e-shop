2b4aeff63738c84ee51b839aaf85a4c67d1e5de8
Rely Arfadillah <shinmendenz@gmail.com>
2023-02-15 12:59:28 +0800

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:e_commerce/screen_size.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        useMaterial3: true,
+      ),
+      home: const ScreenSize(),
+    );
+  }
+}
